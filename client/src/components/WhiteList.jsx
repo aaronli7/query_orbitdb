78d7320d817b23febbd71ef782feba1b73aeb57c
aaronli-uga <ql61608@uga.edu>
2021-10-26 17:57:00 -0400

    update

--- client/src/components/WhiteList.jsx
+++ client/src/components/WhiteList.jsx
@@ -3,12 +3,14 @@ import {useState} from 'react'
 import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 
-import UserTable from '../components/UserTable'
+import UserTable from '@/components/UserTable'
 import axios from 'axios'
+// import getWeb3 from '@/utils/getWeb3'
 
 function WhiteList(){
     return (
-        <h2> Update the white list</h2>
+        <h2> White list</h2>
+        
     )
 }
 
