f54857d13965eac8eb70f0511216807f629b1b5d
aaronli-uga <ql61608@uga.edu>
2021-10-26 21:44:30 -0400

    update add truffle/contract

--- client/src/components/WhiteList.jsx
+++ client/src/components/WhiteList.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import {useState} from 'react'
+import React, { useEffect }from 'react'
+import { useState } from 'react'
 import { useQuery } from 'react-query'
 import { Link } from 'react-router-dom'
 
 import UserTable from '@/components/UserTable'
 import axios from 'axios'
 // import getWeb3 from '@/utils/getWeb3'
+// import Web3 from 'web3'
+
 
 function WhiteList(){
+
+    const [blockChainHash, updateBlockChainHash] = useState(``)
+    const [web3, setWeb3] = useState(``)
+    const [address, updateAddress] = useState(``)
+    const [imgHash, updateImgHash] = useState(``)
+    const [userPK, updateUserPK] = useState(``)
+    const [_storage_name, updateStorageName] = useState(``)
+    const [_AreaID, updateAreaID] = useState(``)
+    const [_PatientID, updatePatientID] = useState(``)
+    const [_WL_requesters, updateWLRequester] = useState(``)
+    const [_remove_WL_requesters, updateRemoveWLRequesters] = useState(``)
+    const [_current_WL, setCurrentWL] = useState(``)
+    const [isWriteSuccess, setWriteSuccess] = useState(false)
+
+    useEffect( () => {
+        console.log("I'm on fire!!!")
+     }, []);
+
     return (
         <h2> White list</h2>
         
